28dd292ab47a5043deafca62acebff8daa5f713d
behzod dev <111036426+behzod-sharifjonov@users.noreply.github.com>
2025-01-19 16:38:32 +0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Navbar from "./Navbar";
+import { FaCirclePlay } from "react-icons/fa6";
+import header_image_1 from '../assets/header_image_1.svg'
+import header_image_2 from '../assets/header_image_2.svg'
+import header_image_3 from '../assets/header_image_3.svg'
+import header_image_4 from '../assets/header_image_4.svg'
+
+function Header() {
+  return (
+    <header>
+      <Navbar />
+      <div className="header-container container">
+        <div className="header-title flex align">
+          <h1>
+            Let’s Learn about new <br /> Knowledge and abilities.
+          </h1>
+          <span>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. <br /> Lorem Ipsum has been the industry's standard dummy
+          </span>
+          <div className="header-btns flex align">
+            <button className="btn">Get Started</button>
+            <button className="header-btn-2 flex align">
+              <FaCirclePlay className="play-icon" />
+              Watch Video
+            </button>
+          </div>
+        </div>
+
+        <div className="header-images flex">
+          <div className="header-img-card">
+            <img src={header_image_1} alt="" />
+          </div>
+          <div className="header-img-card">
+            <img src={header_image_2} alt="" />
+          </div>
+          <div className="header-img-card">
+            <img src={header_image_3} alt="" />
+          </div>
+          <div className="header-img-card">
+            <img src={header_image_4} alt="" />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import Logo from "../assets/Logo.svg";
+import "./components.css";
+import { RiMenu3Fill } from "react-icons/ri";
+
+function Navbar() {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  console.log(toggleMenu);
+
+  return (
+    <nav className="flex align container">
+      <img src={Logo} alt="" />
+      <ul className={`flex nav-ul ${toggleMenu ? "active" : ""} `}>
+        <li>Home</li>
+        <li>Course</li>
+        <li>Monitors</li>
+        <li>Testimonials</li>
+        <li>Blog</li>
+      </ul>
+      <button className="btn">Contact</button>
+      <button className={`nav-menu`} onClick={() => setToggleMenu(!toggleMenu)}>
+        <RiMenu3Fill />
+      </button>
+    </nav>
+  );
+}
+
+export default Navbar;
